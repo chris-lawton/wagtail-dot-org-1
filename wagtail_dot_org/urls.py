724be2e5f84b17e87043a9c9881fd0392d3b2a84
Chris Lawton <chris.lawton@torchbox.com>
2022-07-19 14:45:23 +0100

    comment out repath

--- wagtail_dot_org/urls.py
+++ wagtail_dot_org/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    re_path(r"", include(wagtail_urls)),
+    # re_path(r"", include(wagtail_urls)),
 ]
 
 if apps.is_installed("pattern_library"):
