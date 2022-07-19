3bc7c50e59d7afb13f8e48fe8f7ed0f8e566bca0
Chris Lawton <chris.lawton@torchbox.com>
2022-07-19 15:33:57 +0100

    ensure wagtail urls work

--- wagtail_dot_org/urls.py
+++ wagtail_dot_org/urls.py
@@ -10,16 +10,13 @@ urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    path("pattern-library/", include("pattern_library.urls")),
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    # re_path(r"", include(wagtail_urls)),
+    re_path(r"", include(wagtail_urls)),
 ]
 
-if apps.is_installed("pattern_library"):
-    urlpatterns += [
-        path("pattern-library/", include("pattern_library.urls")),
-    ]
 
 # In development, serve media with the development server
 if settings.SERVE_MEDIA:
