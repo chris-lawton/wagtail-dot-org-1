9e080fb6008da09527f24a5bb3730061e7af2789
Chris Lawton <chris.lawton@torchbox.com>
2022-07-19 14:47:11 +0100

    add project styleguide dir to path

--- wagtail_dot_org/settings.py
+++ wagtail_dot_org/settings.py
@@ -66,7 +66,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(PROJECT_DIR, "templates"),
+            os.path.join(PROJECT_DIR, "project_styleguide/templates"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
